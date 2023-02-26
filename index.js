f3d33f21b21d68f2bbb80e9d0f6348304759b93e
Shubham <shubhamlahase@hmail.com>
2023-02-26 12:21:06 +0530

    new profile route to create protected route

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const cookieParser = require("cookie-parser");
 
 const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
+const profileRoute = require('./routes/profile');
 
 const port = process.env.PORT;
 const url = process.env.MONGO_URI;
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "https://localhost:3000" }))
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
 
 mongoose.set('strictQuery', true);
 mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
@@ -26,8 +27,9 @@ app.get('/test', (req,res) => {
     res.json('express server test ~ OK');
 })
 
-app.use('/api/user', registerRoute);
-app.use('/api/user', loginRoute);
+app.use('/api', registerRoute);
+app.use('/api', loginRoute);
+app.use('/api', profileRoute);
 
 app.listen(port, () => {
     console.log(`Server is running at localhost:${port}`);
